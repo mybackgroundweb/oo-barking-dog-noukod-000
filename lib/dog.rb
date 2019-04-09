b253b4bd3f306fcf1c0005ab474bca29e04bf518
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-09 01:25:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,17 @@
 class Dog
-  def initialize (name, bark = "woof!")
-    @name = dogs_name
-    @bark = bark
-  end
-  
   def name=(dogs_name)
     @name = dogs_name
   end 
-  
-  def bark = (bark)
-    @bark = bark
-  end
 
   def name
     @name
   end 
   
+end 
+
+class Dog
   def bark
-    @bark
-  end
+    puts "woof!"
+  end 
   
 end 
